9721fc203ad5316403121595aea21606661a914c
Sambodhi Katanavre <sambodhikatarnwre@gmail.com>
2026-09-08 22:18:16 +0530

    C programming !

---
+++ RightshiftOpearator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    printf("\n a=%b",a);
+    printf("\n bitwise Rightshift by 1=%d",a>>1);
+}
---
+++ leftshiftOpearator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    printf("\n a=%b");
+    printf("\n bitwise leftshift by 1=%d",a<<1);
+}
